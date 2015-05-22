050715b94938f568e710b26595c141fc2cd9875f
Alex Volkov <git@ulstu.net>
2015-05-22 21:27:25 +0300

    composer

--- funbox/src/funbox/testBundle/Controller/DefaultController.php
+++ funbox/src/funbox/testBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace funbox\testBundle\Controller;
 
 use funbox\testBundle\Entity\CatFood;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -34,6 +35,16 @@ class DefaultController extends Controller
     	return new Response('Created product id '.$CatFood->getId());
     }
 
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $CatFood = $em->getRepository('funboxtestBundle:CatFood')->find($id);
+        $em->remove($CatFood);
+        $em->flush();
+
+        return new Response('CatFood id '.$id.' deleted');
+    }
+
     public function showAllAction()
     {
     	$CatFood = $this->getDoctrine()
@@ -49,9 +60,25 @@ class DefaultController extends Controller
     	}
     }
 
-    public function showAdmin()
+    public function showAdminAction()
     {
-    	return $this->render('funboxtestBundle:Default:admin.html.twig');
+        $CatFood = new CatFood();
+        $CatFood->setMode('default');
+        $CatFood->setTopping('курочкой');
+        $CatFood->setDescription('Hen is awesome!');
+        $CatFood->setQuantity(54);
+        $CatFood->setFooter('Курочка прекрасна!');
+
+        $form = $this->createFormBuilder($CatFood)
+            ->add('mode', 'text')
+            ->add('topping', 'text')
+            ->add('description', 'text')
+            ->add('quantity', 'integer')
+            ->add('footer', 'text')
+            ->add('save', 'submit', array('label' => 'Add Cat Food'))
+            ->getForm();
+
+    	return $this->render('funboxtestBundle:Funbox:admin.html.twig', array('form' => $form->createView(),));
     }
 
     public function editAdmin()
