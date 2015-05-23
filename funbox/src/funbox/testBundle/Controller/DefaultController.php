85bbb0dc4d6bcb8ca0b88cbc2c96b142f0ac6a66
Alexey Volkov <git@ulstu.net>
2015-05-23 16:14:41 +0300

    admin panel 50%

--- funbox/src/funbox/testBundle/Controller/DefaultController.php
+++ funbox/src/funbox/testBundle/Controller/DefaultController.php
@@ -69,38 +69,93 @@ class DefaultController extends Controller
         $CatFood->setQuantity(54);
         $CatFood->setFooter('Курочка прекрасна!');
 
-        $addForm = $this->createFormBuilder($CatFood)
+        $addForm = $this->get('form.factory')->createNamedBuilder('addForm', 'form', $CatFood)
             ->add('mode', 'text')
-            ->add('topping', 'text')
+            ->add('topping', 'text', array('label' => 'с'))
             ->add('description', 'textarea')
             ->add('quantity', 'number')
             ->add('footer', 'textarea')
-            ->add('save', 'submit', array('label' => 'Add Cat Food'))
+            ->add('save', 'submit', array('label' => 'Добавить'))
             ->getForm();
 
-    // submit
-        /*if('POST' === $request->getMethod()) {
- 
-            if ($request->request->has('addForm')) {
-                
-            }
- 
-            if ($request->request->has('removeForm')) {
-            // handle the second form
-            }
-        }*/
-        $addForm->handleRequest($request);
-        if($addForm->isValid()){
-            $em = $this->getDoctrine()->getEntityManager();
-     
-            $em->persist($CatFood);
-            $em->flush();
-     
-            return new Response('Created product id '.$CatFood->getId());
+
+    // remove
+        $em = $this->getDoctrine()->getEntityManager();
+        $CatFood = $em->getRepository('funboxtestBundle:CatFood')->findAll();
+        foreach ($CatFood as $product) {
+            $idList[$product->getId()] = $product->getId();
         }
+        
+        $removeForm = $this->get('form.factory')->createNamedBuilder('removeForm', 'form', $CatFood)
+            ->add('id', 'choice', array('label'=>'Нямушку №','choices' => $idList))
+            ->add('save', 'submit', array('label' => 'Удалить'))
+            ->getForm();
+
+    // edit
+        /*$em = $this->getDoctrine()->getEntityManager();
+        $CatFood = $em->getRepository('funboxtestBundle:CatFood')->findAll();
+        foreach ($CatFood as $product) {
+            $idList[$product->getId()] = $product->getId();
+        }
+        
+        $editForm = $this->get('form.factory')->createNamedBuilder('editForm', 'form', $CatFood)
+            ->add('id', 'choice', array('label'=>'Нямушку №','choices' => $idList))
+            ->add('save', 'submit', array('label' => 'Редактировать'))
+            ->getForm();*/
+        $em = $this->getDoctrine()->getEntityManager();
+        $editForm = $em->getRepository('funboxtestBundle:CatFood')->findAll();
+        foreach ($editForm as $product) {
+            $editFormProducts[] = array(
+                    'id' => $product->getId(),
+                    'mode' => $product->getMode(),
+                    'topping' => $product->getTopping(),
+                    'quantity' => $product->getQuantity()
+                );
+        }
+
+    // submit
+        if($request->isMethod('POST')) {
+        // add
+            $addForm->submit($request->request->get($addForm->getName()));
+            if($addForm->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+     
+                $em->persist($CatFood);
+                $em->flush();
+     
+                return new Response('Created product id '.$CatFood->getId());
+            }
+        // remove
+            $removeForm->submit($request->request->get($removeForm->getName()));
+            if($removeForm->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+                $id = $request->request->get($removeForm->getName())['id'];
+     
+                $CatFood = $em->getRepository('funboxtestBundle:CatFood')->find($id);
+                $em->remove($CatFood);
+                $em->flush();
+     
+                return new Response('Product #'.$id.' has been deleted');
+            }
+        // edit
+            $editForm->submit($request->request->get($editForm->getName()));
+            if($editForm->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+                $id = $request->request->get($editForm->getName())['id'];
+     
+                $CatFood = $em->getRepository('funboxtestBundle:CatFood')->find($id);
+                $em->remove($CatFood);
+                $em->flush();
+     
+                return new Response('Product #'.$id.' has been deleted');
+            }
+        }
+
+    //render
         return $this->render('funboxtestBundle:Funbox:admin.html.twig', array(
-            'addForm' => $addForm->createView()/*,
-            'removeForm' => $removeForm->createView()*/
+            'addForm' => $addForm->createView(),
+            'removeForm' => $removeForm->createView(),
+            'editFormProducts' => $editFormProducts
             ));
         
     }
